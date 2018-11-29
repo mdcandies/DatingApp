a98bd2a269a4902e72ff8612e759d7cf097dae81
Mike Candies <mdcandies@verizon.net>
2018-11-29 10:45:40 -0500

    End of Section 17

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -83,10 +83,17 @@ namespace DatingApp.API
             }
 
             // app.UseHttpsRedirection();
-            //seeder.SeedUsers();
+            // seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
-            app.UseMvc();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Fallback", action = "Index" }
+                );
+            });
         }
     }
 }
